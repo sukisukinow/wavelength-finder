b51aef4ca4a126233e44451ea66940667892c60a
sukisukinow <139166847+sukisukinow@users.noreply.github.com>
2023-10-26 12:53:43 -0400

    Update updated_Wavelength_finder.py

--- updated_Wavelength_finder.py
+++ updated_Wavelength_finder.py
@@ -8,47 +8,40 @@ Created on Thu Aug 17 14:42:52 2023
 import os
 from statistics import mode
 
-directory = 'C:\sedfit\user data\Life Edit\2023172\25OCT2023\155050'
+directory = r'C:\sedfit\user data\Life Edit\2023172\25OCT2023\155050'
 
 file_list= os.listdir(directory)
 
+#this lets the user choose how many files they want to include
+first_scan_index=int(input("What is the number of the first scan? "))
+last_scan_index=int(input("What is the number of the last scan? "))
+
+#this makes a list of channels that can be used as the input for the for loop over channels 
+ra_list=input("what are the channels used (please write them as a list of integers separated by commas eg. 1,2,3 )")
+channels = [int(channel) for channel in ra_list.split(',')]
 
 
-#Save the total
-running_total = 0
+#initialize an empty wavelength list
+wavelengths = []
 
-#Loops over filename numbers from 1 to 20
-for i in range(1, 20+1):
-               #For each filename number, gets the ra1, ra2, ra3 
-               for j in range(1, 3+1):
-                              #Turns the filename number into a string
-                              filename_string = str(i)
-                              #Creates a string of zeros, assuming each filename has 5 total characters
-                              filename_chars = 5
-                              zero_string = "".join(["0"]*(filename_chars-len(filename_string)))
-                              #Gets the extension string
-                              extension_string = ".RA" + str(j)
+#For each filename number, gets the ra1, ra2, ra3 
+for channel in channels:
+    for scan_index in range(first_scan_index, last_scan_index+1):
+               
+             # Create the filename based on channel and scan_index
+        filename = f"{scan_index:05d}.RA{channel}"
+        print(filename)
+        full_path = os.path.join(directory, filename)
 
-                              #Creates the filename string
-                              filename_string = zero_string + filename_string + extension_string
+        if os.path.exists(full_path):
+            # Open the file and read the second line
+            with open(full_path, "r") as f:
+                file_lines = f.readlines()
+                second_line = file_lines[1]
+                second_line_list = second_line.split()
+                second_line_second_last = second_line_list[-2]
+                file_number = int(second_line_second_last)
+                wavelengths.append(file_number)
 
-                              #Opens the file
-                              f = open(filename_string, "r")
-                              file_lines = f.readlines()
-                              second_line = file_lines[1]
-
-                              #Gets the line number as a string
-                              second_line_list = second_line.split()
-                              second_line_second_last = second_line_list[-2]
-
-                              #Converts the line number to a number and adds to the running total
-                              file_number = int(second_line_second_last)
-                              
-                              set=[]
-                              set.append(file_number)
-
-                              running_total = running_total+file_number
-
-#20 filenumbers, 3 extensions per file means 60 items should have been gotten
-setmode =  mode(set)
+setmode = mode(wavelengths)
 print(setmode)
